dc70e3b9a7813a7eaaebfa79625f0b8d6a9b488e
vincent-tsui <ccheat@qq.com>
2016-01-20 18:02:44 +0800

    sharejs v0.1
    title: 'string',
    desc: 'string',
    shareUrl: 'string',
    imgUrl: 'string',
    goToUrl: 'string',
    from: 'string'

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
 # sharejs
-微信分享组件
+`sharejs` 是微信分享的前端组件，它可以快速对接后端的微信分享逻辑，并在不同页面传入不同的 `option` 即可分别分享不同的内容。
+## Basic Usage
+1. 引入微信分享接口和`sharejs`到你的页面中
+	
+    ```html
+    <script type="text/javascript" src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+	<script type="text/javascript" src="share.js"></script>
+    ```
+
+2. 修改`sharejs`中的配置对象`initConfig`，initConfig详细说明见下文API处
+
+3. 执行`share()`方法并传入`option`，option详细说明见下文API处
+
+    ```javascript
+    share({
+		title: 'string',
+		desc: 'string',
+		shareUrl: 'string',
+		imgUrl: 'string',
+		goToUrl: 'string',
+		from: 'string'
+	});
+    ```
+
+4. 现在你可以在微信中分享你的页面了
+
+## API
+
+### initConfig配置说明
+
+initConfig例子：
+```javascript
+var initConfig = {
+    getConfigPath : '/wxShareAjax',
+    defaultImgUrl : 'weixindefault.jpg',
+    secondObject : 'msg',
+    appIdKey : 'appId',
+    timestampKey : 'timestamp',
+    nonceStrKey : 'noncestr',
+    signatureKey : 'signature',
+};
+```
+
+服务器返回的json例子：
+```json
+{"error":0,"msg":{"signature":"123qwe123qwe","noncestr":"abc","timestamp":1453279155,"appId":"wxxxxxx","url":"http:\/\/xxx.com"}}
+```
+
+* getConfigPath 必填，string，获取微信分享各个参数的后端AJAX接口路径，例如：'/wxShareAjax'
+* defaultImgUrl 必填，string，分享的图标缺省时，显示的缺省图标url，例如：'weixindefault.jpg'
+* secondObject 选填，string，后端返回的json数据，若在二级属性中，则在此填二级属性名，若在一级则不填。若如上述json所示，则此处填写'msg'
+* appIdKey：必填，string，返回json中appid的键名。若如上述json所示，则此处填写'appId'
+* timestampKey：必填，string，返回json中时间戳的键名。若如上述json所示，则此处填写'timestamp'
+* nonceStrKey：必填，string，返回json中随机字符串的键名。若如上述json所示，则此处填写'noncestr'
+* signatureKey：必填，string，返回json中签名的键名。若如上述json所示，则此处填写'signature'
+
+### option配置说明
+
+option例子：
+```javascript
+share({
+	title: 'string',
+	desc: 'string',
+	shareUrl: 'string',
+	imgUrl: 'string',
+	goToUrl: 'string',
+	from: 'string'
+});
+```
+* title 必填，string，分享的标题
+* desc 必填，string，分享的描述
+* shareUrl 选填，string，分享的链接，若不填则分享当前页
+* imgUrl 选填，string，分享的图标链接，若不填则显示缺省图标
+* goToUrl 选填，string，分享成功后跳转到的链接
+* from 选填，string，分享成功后计数的ajax链接，每次成功分享都会请求一次此ajax接口
---
+++ share.js
@@ -0,0 +1,92 @@
+/*
+@   Name：微信分享公共组件
+@   version：0.1
+@   Author：vincent-tsui
+@   Mail: admin@xcje.cn
+@   Note: 先配置initConfig，然后执行share(option)
+@   param title 分享的标题
+@   param desc 分享的描述
+@   param shareUrl 分享出去的链接，为空则分享出去当前页的链接
+@   param imgUrl 分享的图标链接，为空则图标为银巴克LOGO
+@   param goToUrl 分享后跳转的链接，为空则不跳转
+@   param from 统计分享来源的ajax接口url，无则留空
+@
+@   example:
+@
+@   share({
+@       title: 'string',
+@       desc: 'string',
+@       shareUrl: 'string',
+@       imgUrl: 'string',
+@       goToUrl: 'string',
+@       from: 'string'
+@   });
+@
+*/
+var initConfig = {
+    getConfigPath : '/wxShareAjax',
+    defaultImgUrl : 'http://photocdn.sohu.com/20140808/Img403237988.jpg',
+    secondObject : 'msg',
+    appIdKey : 'appId',
+    timestampKey : 'timestamp',
+    nonceStrKey : 'noncestr',
+    signatureKey : 'signature',
+};
+function share(option){
+    function encode(url) {
+        return encodeURIComponent(url).replace(/'/g, "%27").replace(/"/g, "%22");
+    }
+    var getConfigUrl = initConfig.getConfigPath + '?url=' + encode(window.location.href),
+        shareUrl = option.shareUrl ? option.shareUrl : window.location.href,
+        imgUrl = option.imgUrl ? option.imgUrl : initConfig.defaultImgUrl;
+    var callback = function (data) {
+        data = data[initConfig.secondObject] || data;
+        wx.config({
+            debug: false,
+            appId: data[initConfig.appIdKey],
+            timestamp: data[initConfig.timestampKey],
+            nonceStr: data[initConfig.nonceStrKey],
+            signature: data[initConfig.signatureKey],
+            jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareWeibo']
+        });
+        wx.ready(function () {
+            var shareData = {
+                title: option.title,
+                desc: option.desc,
+                link: shareUrl, // 分享链接
+                imgUrl: imgUrl,
+                success: function () {
+                    if (option.from) {
+                        $.post(option.from)
+                    };
+                    // 用户确认分享后执行的回调函数
+                    if(option.goToUrl){
+                        window.location.href = option.goToUrl;
+                    }
+                }
+            };
+            wx.onMenuShareAppMessage(shareData);
+            wx.onMenuShareTimeline(shareData);
+            wx.onMenuShareQQ(shareData);
+            wx.onMenuShareWeibo(shareData);
+        });
+        wx.error(function (res) {
+            //alert(res.errMsg);
+        });
+    };
+    $.ajax({
+        url: getConfigUrl,
+        dataType: "json",
+        success: function (response) {
+            callback && callback(response);
+        },
+        error: function (xhr, type) {
+            console.log('xhr:');
+            console.log(xhr);
+            console.log('type:');
+            console.log(type);
+            console.log("网络错误")
+        }
+    })
+}
+    
